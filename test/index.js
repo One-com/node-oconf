2acd6c5b907b16bcb0f20101d3e668cc6c68cdbf
Gustav Nikolaj Olsen <gno@one.com>
2015-01-20 14:51:52 +0100

    allow ignoring by globs

--- test/index.js
+++ test/index.js
@@ -76,4 +76,26 @@ describe('Basic tests', function () {
             }, 'not to throw');
         });
     });
+    describe('ignore included files with globbing', function () {
+        it('should not throw an error when ignoring by glob #1', function () {
+            expect(function () {
+                oconf.load(resolve('./files/includeNonExistentFile.cjson'), { ignore: '**/*.cjson' });
+            }, 'not to throw');
+        });
+        it('should not throw an error when ignoring by glob #2', function () {
+            expect(function () {
+                oconf.load(resolve('./files/includeNonExistentFile.cjson'), { ignore: resolve('./**') });
+            }, 'not to throw');
+        });
+        it('should not throw an error when ignoring by glob', function () {
+            expect(function () {
+                oconf.load(resolve('./files/includeNonExistentFile.cjson'), { ignore: resolve('./files/*') });
+            }, 'not to throw');
+        });
+        it('should throw an error when ignoring by glob but including file not matched by glob', function () {
+            expect(function () {
+                oconf.load(resolve('./files/includeNonExistentFile.cjson'), { ignore: resolve('./otherfiles/*.cjson') });
+            }, 'to throw', /^ENOENT, no such file or directory/);
+        });
+    });
 });
