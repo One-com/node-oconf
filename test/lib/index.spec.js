0550a5e94c69406e28a4e77841fd6b504aeb208c
Gustav Nikolaj Olsen <gno@one.com>
2015-01-28 23:22:10 +0100

    move test file

--- test/lib/index.spec.js
+++ test/lib/index.spec.js
@@ -1,17 +1,17 @@
 /*global describe, it, before*/
 var expect = require('unexpected'),
-    oconf = require('../lib/index'),
+    oconf = require('../../lib/index'),
     path = require('path');
 
-function resolve(filename) {
-    return path.resolve(__dirname, filename);
+function testFile(filename) {
+    return require('path').resolve(__dirname, '..', 'files', filename);
 }
 
 describe('Basic tests', function () {
     describe('base.cjson', function () {
         var data;
         before(function () {
-            data = oconf.load(resolve('./files/base.cjson'));
+            data = oconf.load(testFile('base.cjson'));
         });
         it('Has foo = "overwrite this"', function () {
             expect(data, 'to have property', 'foo', 'overwrite this');
@@ -26,7 +26,7 @@ describe('Basic tests', function () {
     describe('extend-base.cjson', function () {
         var data;
         before(function () {
-            data = oconf.load(resolve('./files/extend-base.cjson'));
+            data = oconf.load(testFile('extend-base.cjson'));
         });
         it('Has foo = "bar" (i.e. overwritten)', function () {
             expect(data, 'to have property', 'foo', 'bar');
@@ -41,7 +41,7 @@ describe('Basic tests', function () {
     describe('extend-in-subobject.cjson', function () {
         var data;
         before(function () {
-            data = oconf.load(resolve('./files/extend-in-subobject.cjson'));
+            data = oconf.load(testFile('extend-in-subobject.cjson'));
         });
         it('Has subobject = contents of extend-base.cjson', function () {
             expect(data, 'to equal', {
@@ -55,7 +55,7 @@ describe('Basic tests', function () {
     describe('extend-multiple.cjson', function () {
         var data;
         before(function () {
-            data = oconf.load(resolve('./files/extend-multiple.cjson'));
+            data = oconf.load(testFile('extend-multiple.cjson'));
         });
         it('should have properties from both extended files', function () {
             expect(data, 'to equal', {
@@ -67,7 +67,7 @@ describe('Basic tests', function () {
     describe('extend-nested.cjson', function () {
         var data;
         before(function () {
-            data = oconf.load(resolve('./files/extend-nested.cjson'));
+            data = oconf.load(testFile('extend-nested.cjson'));
         });
         it('should have properties from both extended files', function () {
             expect(data, 'to equal', {
@@ -80,7 +80,7 @@ describe('Basic tests', function () {
     describe('list-of-includes.cjson', function () {
         var data;
         before(function () {
-            data = oconf.load(resolve('./files/list-of-includes.cjson'));
+            data = oconf.load(testFile('list-of-includes.cjson'));
         });
         it('should resolve includes in the list', function () {
             expect(data, 'to equal', [
@@ -91,51 +91,51 @@ describe('Basic tests', function () {
     describe('loop1.cjson / loop2.cjson', function () {
         it('Loading loop1.cjson throws error', function () {
             expect(function () {
-                oconf.load(resolve('./files/loop1.cjson'));
+                oconf.load(testFile('loop1.cjson'));
             }, 'to throw', /^Loop in loaded files: /);
         });
         it('Loading loop2.cjson throws error', function () {
             expect(function () {
-                oconf.load(resolve('./files/loop2.cjson'));
+                oconf.load(testFile('loop2.cjson'));
             }, 'to throw', /^Loop in loaded files:/);
         });
     });
     describe('includeNonExistentFile.cjson', function () {
         it('should throw an error when nonExistentFile.cjson is not ignored', function () {
             expect(function () {
-                oconf.load(resolve('./files/includeNonExistentFile.cjson'));
+                oconf.load(testFile('includeNonExistentFile.cjson'));
             }, 'to throw', /^ENOENT, no such file or directory/);
         });
         it('should not throw an error when nonExistentFile.cjson is ignored', function () {
             expect(function () {
-                oconf.load(resolve('./files/includeNonExistentFile.cjson'), {ignore: resolve('./files/nonExistentFile.cjson')});
+                oconf.load(testFile('includeNonExistentFile.cjson'), {ignore: testFile('nonExistentFile.cjson')});
             }, 'not to throw');
         });
     });
     describe('ignore included files with globbing', function () {
         it('should not throw an error when ignoring by glob #1', function () {
             expect(function () {
-                oconf.load(resolve('./files/includeNonExistentFile.cjson'), { ignore: '**/*.cjson' });
+                oconf.load(testFile('includeNonExistentFile.cjson'), { ignore: '**/*.cjson' });
             }, 'not to throw');
         });
         it('should not throw an error when ignoring by glob #2', function () {
             expect(function () {
-                oconf.load(resolve('./files/includeNonExistentFile.cjson'), { ignore: resolve('./**') });
+                oconf.load(testFile('includeNonExistentFile.cjson'), { ignore: testFile('./**') });
             }, 'not to throw');
         });
         it('should not throw an error when ignoring by glob #3', function () {
             expect(function () {
-                oconf.load(resolve('./files/includeNonExistentFile.cjson'), { ignore: resolve('./files/*') });
+                oconf.load(testFile('includeNonExistentFile.cjson'), { ignore: testFile('../files/*') });
             }, 'not to throw');
         });
         it('should not throw an error when ignoring by glob #4', function () {
             expect(function () {
-                oconf.load(resolve('./files/includeNonExistentFile.cjson'), { ignore: 'files/*.cjson', cwd: __dirname });
+                oconf.load(testFile('includeNonExistentFile.cjson'), { ignore: '../files/*.cjson', cwd: __dirname });
             }, 'not to throw');
         });
         it('should throw an error when ignoring by glob but including file not matched by glob', function () {
             expect(function () {
-                oconf.load(resolve('./files/includeNonExistentFile.cjson'), { ignore: resolve('./otherfiles/*.cjson') });
+                oconf.load(testFile('includeNonExistentFile.cjson'), { ignore: testFile('foo/*cjson') });
             }, 'to throw', /^ENOENT, no such file or directory/);
         });
     });
