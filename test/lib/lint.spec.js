f1838449081f728bb5c2316e6f30b75ade9b968a
Gustav Nikolaj Olsen <gno@one.com>
2015-03-01 14:39:36 +0100

    refactor tests

--- test/lib/lint.spec.js
+++ test/lib/lint.spec.js
@@ -1,9 +1,16 @@
 /* global describe, it */
-var lint = require('../../lib/lint');
-var expect = require('unexpected');
 var Path = require('path');
+var lint = require('../../lib/lint');
+var expect = require('unexpected')
+    .clone()
+    .addAssertion('to be an error', function (expect, subject, value) {
+        expect(subject, 'to be an', Error);
+        expect(subject, 'to satisfy', {
+            message: value
+        });
+    });
 
-function testFile(filename) {
+function testData(filename) {
     return Path.resolve(__dirname, '..', 'files', 'lint', filename);
 }
 
@@ -12,18 +19,21 @@ describe('lib/lint', function () {
     it('should export a function', function () {
         expect(lint, 'to be a function');
     });
+    it('should throw an error if path does not exist', function (done) {
+        lint(testData('nonExistant.cjson'), function (err) {
+            expect(err, 'to be an error', /^ENOENT/);
+            done();
+        });
+    });
     it('should not complain when given a valid file', function (done) {
-        lint(testFile('valid.cjson'), function (err) {
+        lint(testData('valid.cjson'), function (err) {
             expect(err, 'to be undefined');
             done();
         });
     });
     it('should complain when given an invalid file', function (done) {
-        lint(testFile('invalid.cjson'), function (err) {
-            expect(err, 'to be an', Error);
-            expect(err, 'to satisfy', {
-                message: /^Parse error on line 2:/
-            });
+        lint(testData('invalid.cjson'), function (err) {
+            expect(err, 'to be an error', /^Parse error on line 2:/);
             done();
         });
     });
