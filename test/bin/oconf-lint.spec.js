f56c666a976ff3b853def894952391e027deb5c5
Gustav Nikolaj Olsen <gno@one.com>
2015-04-22 19:51:02 +0200

    rewrite lint tests to use custom assertion

--- test/bin/oconf-lint.spec.js
+++ test/bin/oconf-lint.spec.js
@@ -4,32 +4,65 @@ var expect = require('unexpected');
 
 var pathToBin = require('path').resolve(__dirname, '../../bin/', 'oconf-lint');
 
+expect.addAssertion('when oconf-linted', function (expect, subject, value) {
+    this.errorMode = 'nested';
+    var that = this;
+    return expect.promise(function (resolve, reject) {
+        var args;
+        if (typeof subject === 'string') {
+            args = subject;
+        } else {
+            if (Array.isArray(subject)) {
+                args = subject.join(' ');
+            } else {
+                return reject(new Error('arguments must be supplied as string or array'));
+            }
+        }
+
+        exec(pathToBin + ' ' + args, function (err, stdout, stderr) {
+            resolve({
+                err: err,
+                code: err && err.code || 0,
+                stdout: stdout,
+                stderr: stderr
+            });
+        });
+    }).then(function (value) {
+        return that.shift(value, 0);
+    });
+});
+
+function testFile(fileName) {
+    if (!(/\.cjson$/.test(fileName))) {
+        fileName += '.cjson';
+    }
+    return require('path').resolve(__dirname, '..', 'files', fileName);
+}
+
 describe('bin/oconf-lint', function () {
-    it('should exit with no errors if valid', function (done) {
-        var testFile = require('path').resolve(__dirname, '..', 'files', 'lint', 'valid.cjson');
-        exec(pathToBin + ' ' + testFile, function (err, stdout, stderr) {
-            expect(err, 'to be null');
-            expect(stdout, 'to equal', '');
-            expect(stderr, 'to equal', 'No linting errors found.\n');
-            done();
+    it('should exit with no errors if valid', function () {
+        return expect(testFile('lint/valid'), 'when oconf-linted', 'to satisfy', {
+            err: null,
+            code: 0,
+            stdout: '',
+            stderr: 'No linting errors found.\n'
         });
     });
-    it('should report the errors if invalid', function (done) {
-        var testFile = require('path').resolve(__dirname, '..', 'files', 'lint', 'invalid.cjson');
-        exec(pathToBin + ' ' + testFile, function (err, stdout, stderr) {
-            expect(err, 'to be an', Error);
-            expect(err, 'to have property', 'code', 1);
-            expect(stdout, 'to equal', '');
-            expect(stderr, 'to equal', [
-                testFile,
+    it('should report the errors if invalid', function () {
+        var filePath = testFile('lint/invalid');
+        return expect(filePath, 'when oconf-linted', 'to satisfy', {
+            err: expect.it('to be an', Error),
+            code: 1,
+            stdout: '',
+            stderr: [
+                filePath,
                 '',
                 'Parse error on line 2:',
                 '{    "foo": "bar}',
                 '------------^',
                 "Expecting 'STRING', 'NUMBER', 'NULL', 'TRUE', 'FALSE', '{', '[', got 'undefined'",
                 ''
-            ].join('\n'));
-            done();
+            ].join('\n')
         });
     });
 });
